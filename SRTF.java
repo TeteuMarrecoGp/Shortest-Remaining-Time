3370f2dc07144f52346a8da01867af293f627bc8
TeteuMarrecoGp <43143711+TeteuMarrecoGp@users.noreply.github.com>
2018-09-10 17:57:21 -0300

    Enviado por Matheus

--- SRTF.java
+++ SRTF.java
@@ -1,4 +1,22 @@
- System.out.println();
+package escalonamento;
+
+import java.io.*;
+public class SRTF {
+ public static void main(String args[]) throws IOException
+ {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n;
+      System.out.println("Por favor digite o número de processos: ");
+       n = Integer.parseInt(br.readLine());
+       int proc[][] = new int[n + 1][4];
+       for(int i = 1; i <= n; i++)
+       {
+      System.out.println("Por favor, informe o horário de chegada do processo " + i + ": ");
+      proc[i][0] = Integer.parseInt(br.readLine());
+      System.out.println("Por favor, informe o tempo de burst para o processo " + i + ": ");
+      proc[i][1] = Integer.parseInt(br.readLine());
+     }
+   System.out.println();
      
        int total_time = 0;
      for(int i = 1; i <= n; i++)
@@ -21,24 +39,7 @@
          sel_proc = j;
         }
        }
-      }package escalonamento;
-
-import java.io.*;
-public class SRTF {
- public static void main(String args[]) throws IOException
- {
-  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n;
-      System.out.println("Por favor digite o número de processos: ");
-       n = Integer.parseInt(br.readLine());
-       int proc[][] = new int[n + 1][4];
-       for(int i = 1; i <= n; i++)
-       {
-      System.out.println("Por favor, informe o horário de chegada do processo " + i + ": ");
-      proc[i][0] = Integer.parseInt(br.readLine());
-      System.out.println("Por favor, informe o tempo de burst para o processo " + i + ": ");
-      proc[i][1] = Integer.parseInt(br.readLine());
-     }
+      }
   for(int j = 1; j <= n; j++)
       {
        if(proc[j][0] <= i)
